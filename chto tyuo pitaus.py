c1ec30c856eddfcb6a587cb275d6065fd9358cc9
fufus131 <102593554+fufus131@users.noreply.github.com>
2022-04-19 20:26:12 +0700

    Add files via upload

---
+++ chto tyuo pitaus.py
@@ -0,0 +1,8 @@
+n1 = input("Ведите число:")
+n1= int(n1)
+n = input("Ведите число:")
+n= int(n)
+if  n1<n:
+  print (n1)
+if n<n1:
+  print(n)
---
+++ букавы от 1 до 0.py
@@ -0,0 +1,24 @@
+import keyboard
+while True:
+    if keyboard.is_pressed('1') == True:
+        print('a')
+    if keyboard.is_pressed('2') == True:
+        print('б')
+    if keyboard.is_pressed('3') == True:
+        print('в')
+    if keyboard.is_pressed('4') == True:
+        print('г')
+    if keyboard.is_pressed('5') == True:
+        print('д')
+    if keyboard.is_pressed('6') == True:
+        print('е')
+    if keyboard.is_pressed('7') == True:
+        print('ё')
+    if keyboard.is_pressed('8') == True:
+        print('ж')
+    if keyboard.is_pressed('9') == True:
+        print('з')
+    if keyboard.is_pressed('0') == True:
+        print('Конец!')   
+        break
+
