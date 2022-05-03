b9c05b763b2afc163bf043708a70684031ea6c83
fufus131 <102593554+fufus131@users.noreply.github.com>
2022-05-03 18:58:56 +0700

    Add files via upload

---
+++ логин парооль в одном цикле.py
@@ -0,0 +1,7 @@
+login = input('Ведите Логин:')
+parol = input('Ведите Пароль:')
+while login = 'fufus' or parol = '7689':
+print('неверный логин или пароль')
+    login = input('Ведите Логин:')
+    parol = input('Ведите Пароль:')
+print('вы вошли в аккаунт')
