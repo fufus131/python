6d86ab49aff828302f092db9f9c0d5a28228c6c2
fufus131 <102593554+fufus131@users.noreply.github.com>
2022-05-24 20:18:59 +0700

    Update kakalyator В2.py

--- kakalyator В2.py
+++ kakalyator В2.py
@@ -1,3 +1,10 @@
+login = input('Ведите Логин:')
+parol = input('Ведите Пароль:')
+while login != 'fufus' or parol != '7689':
+    print('неверный логин или пароль')
+    login = input('Ведите Логин:')
+    parol = input('Ведите Пароль:')
+print('вы вошли в аккаунт')
 vibiri = input('Выберите+,-,*,/')
 while vibiri !="exit":
     if vibiri == 'kraborifma':
